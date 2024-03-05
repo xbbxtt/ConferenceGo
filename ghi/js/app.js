916aff8cd7a262645034cfba7383c6eabe160680
Shiran <xiao.shiran@gmail.com>
2024-03-04 23:33:49 -0500

    adding card footer and time

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,4 @@
-function createCard(name, description, pictureUrl) {
+function createCard(name, description, pictureUrl, startTime, endTime) {
     return `
     <div class="col-md-4" class="shadow p-3 mb-5 bg-white rounded">
         <div class="shadow p-0 mb-3 bg-white rounded">
@@ -7,6 +7,9 @@ function createCard(name, description, pictureUrl) {
                 <div class="card-body">
                 <h5 class="card-title">${name}</h5>
                 <p class="card-text">${description}</p>
+                </div>
+                <div class="card-footer">
+                ${startTime} - ${endTime}
                 </div>
             </div>
         </div>
@@ -33,12 +36,18 @@ window.addEventListener('DOMContentLoaded', async () => {
 
                 const detailUrl = `http://localhost:8000${conference.href}`;
                 const detailResponse = await fetch(detailUrl);
+
                 if (detailResponse.ok) {
                     const details = await detailResponse.json();
+
+                    console.log(details)
+
                     const description = details.conference.description;
                     const title = details.conference.name
                     const pictureUrl = details.conference.location.picture_url;
-                    const html = createCard(title, description, pictureUrl)
+                    const startTime = new Date(details.conference.starts).toLocaleDateString('en-US')
+                    const endTime = new Date(details.conference.ends).toLocaleDateString('en-US')
+                    const html = createCard(title, description, pictureUrl, startTime, endTime)
 
                     const column = document.querySelector('.row')
                     column.innerHTML += html
